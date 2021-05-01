4aae6069b6939f9c8ea9db4733ac90d5ee42237a
Deepesh Padala <d33p@arch.localdomain>
2021-05-01 17:30:32 +0530

    multiple votes fixed, able to change vote

--- server.js
+++ server.js
@@ -9,7 +9,6 @@ let users = structures.users;
 let videos = structures.videos;
 let reports = structures.reports;
 
-
 app.get("/api/reports", (req, res) => {
     const url = req.query.url;
     console.log(`checking ${url}`);
@@ -56,7 +55,7 @@ app.post("/api/vote", (req, res) => {
         url = req.body.url;
         report_id = req.body.reportId;
         score = req.body.score;
-        email_id = req.body.email_id;
+        email_id = req.body.emailId;
         if (
             url === undefined ||
             report_id === undefined ||
@@ -79,50 +78,112 @@ app.post("/api/vote", (req, res) => {
             console.log("Video Found", url);
             users.is_user_there(email_id, (user_is_there, user_id) => {
                 if (user_is_there) {
-                    if (score === 1) {
-                        users.add_vote(user_id, "upvoted_reviews", video_id, report_id);
-                        reports.upvote(
-                            video_id,
-                            report_id,
-                            () => {
-                                // Success Callback
-                                res.status(200).json({
-                                    message: "Successfully Upvoted",
+                    users.is_report_voted_on(user_id, video_id, report_id, (vote_exists, vote_collection, vote_id) => {
+                        if (!vote_exists) {
+                            if (score === 1) {
+                                users.add_vote(user_id, "upvoted_reports", video_id, report_id);
+                                reports.upvote(
+                                    video_id,
+                                    report_id,
+                                    () => {
+                                        // Success Callback
+                                        res.status(200).json({
+                                            message: "Successfully Upvoted",
+                                        });
+                                    },
+                                    (error) => {
+                                        // Error Callback
+                                        console.log(error);
+                                        res.status(500).json({ message: error.message });
+                                    }
+                                );
+                            } else if (score === -1) {
+                                users.add_vote(user_id, "downvoted_reports", video_id, report_id);
+                                reports.downvote(
+                                    video_id,
+                                    report_id,
+                                    () => {
+                                        // Success Callback
+                                        res.status(200).json({
+                                            message: "Successfully Downvoted",
+                                        });
+                                    },
+                                    (error) => {
+                                        // Error Callback
+                                        console.log(error);
+                                        res.status(500).json({ message: error.message });
+                                    }
+                                );
+                            } else {
+                                res.status(400).json({
+                                    message: "Invalid Score. Score should be +1/-1",
                                 });
-                            },
-                            (error) => {
-                                // Error Callback
-                                console.log(error);
-                                res.status(500).json({ message: error.message });
-                            }
-                        );
-                    } else if (score === -1) {
-                        users.add_vote(user_id, "downvoted_reviews", video_id, report_id);
-                        reports.downvote(
-                            video_id,
-                            report_id,
-                            () => {
-                                // Success Callback
-                                res.status(200).json({
-                                    message: "Successfully Downvoted",
+                            }                
+                        } else if ((vote_collection == "upvoted_reports" && score === +1) ||
+                                    (vote_collection == "downvoted_reports" && score === -1)) {
+                                        console.log(`was already ${vote_collection}`);
+                                        res.status(500).json({message: "already voted on this"});
+                        } else {
+                            if (vote_collection == "downvoted_reports") {
+                                console.log("was downvoted, upvoting now");
+                                reports.upvote(video_id, report_id, () => {
+                                    console.log("upvoted successfully, now to reduce downvote");
+                                    reports.reduce_downvote(video_id, report_id, () => {
+                                        console.log("reduced downvotes succesfully");
+                                    }, (error) => {
+                                        console.error("couldn't reduce downvotes", error);
+                                    });
+                                }, (error) => {
+                                    console.error("couldn't upvote", error);
                                 });
-                            },
-                            (error) => {
-                                // Error Callback
-                                console.log(error);
-                                res.status(500).json({ message: error.message });
+                                users.add_vote(user_id, "upvoted_reports", video_id, report_id);
+                                res.status(200).json({message: "changed vote to upvote"});
+                            } else {
+                                console.log("was upvoted, downvoting now");
+                                reports.downvote(video_id, report_id, () => {
+                                    console.log("downvoted successfully, now to reduce upvotes");
+                                    reports.reduce_upvote(video_id, report_id, () => {
+                                        console.log("reduced upvotes");
+                                    }, (error) => {
+                                        console.error("couldn't reduce upvotes", error);
+                                    });
+
+                                }, (error) => {
+                                    console.error("couldn't downvote, ran into some error", error);
+                                });
+                                users.add_vote(user_id, "downvoted_reports", video_id, report_id);
+                                console.log("added vote in users database");
+                                res.status(200).json({message: "changed vote to downvote"});
                             }
-                        );
-                    } else {
-                        res.status(400).json({
-                            message: "Invalid Score. Score should be +1/-1",
-                        });
-                    }        
+                        }
+                    });
                 }
             });
         } else {
             res.status(400).json({ message: "No Such Video found" });
         }
+    });
+});
+
+
+app.get('/api/uservoted', (req, res) => {
+    let user_email = req.query.email;
+    let video_id = req.query.video_id;
+    let report_id = req.query.report_id;
+    users.is_user_there(user_email, (_user_is_there, user_id) => {
+        users.is_report_voted_on(user_id, video_id, report_id, (vote_exists, collection, _vote_id) => {
+            if (vote_exists) {
+                res.json({
+                    voted: true,
+                    collection: collection
+                });
+            } else {
+                res.json({
+                    voted: false,
+                    collection: null
+                });
+            }
+        });
     });
 });
 
--- structures.js
+++ structures.js
@@ -62,6 +62,41 @@ class Reports {
             error_callback(error);
         }
     }
+    reduce_upvote(video_id, report_id, success_callback, error_callback) {
+        try {
+            firestore_client.db
+                .collection("videos")
+                .doc(video_id)
+                .collection("reports")
+                .doc(report_id)
+                .get()
+                .then(async (doc) => {
+                    let current_data = await doc.data();
+                    if (current_data === undefined) {
+                        try {
+                            console.log(current_data.num_upvotes);
+                        } catch (error) {
+                            error_callback(error);
+                        }
+                        return;
+                    }
+                    current_data.num_upvotes -= 1;
+                    firestore_client.db
+                        .collection("videos")
+                        .doc(video_id)
+                        .collection("reports")
+                        .doc(report_id)
+                        .set(current_data)
+                        .then(success_callback)
+                        .catch((error) => {
+                            error_callback(error);
+                        });
+                });
+        } catch (error) {
+            error_callback(error);
+        }
+    }
+
 
     downvote(video_id, report_id, success_callback, error_callback) {
         try {
@@ -97,6 +132,41 @@ class Reports {
             error_callback(error);
         }
     }
+    reduce_downvote(video_id, report_id, success_callback, error_callback) {
+        try {
+            firestore_client.db
+                .collection("videos")
+                .doc(video_id)
+                .collection("reports")
+                .doc(report_id)
+                .get()
+                .then(async (doc) => {
+                    let current_data = await doc.data();
+                    if (current_data === undefined) {
+                        try {
+                            console.log(current_data.num_upvotes);
+                        } catch (error) {
+                            error_callback(error);
+                        }
+                        return;
+                    }
+                    current_data.num_downvotes -= 1;
+                    firestore_client.db
+                        .collection("videos")
+                        .doc(video_id)
+                        .collection("reports")
+                        .doc(report_id)
+                        .set(current_data)
+                        .then(success_callback)
+                        .catch((error) => {
+                            error_callback(error);
+                        });
+                });
+        } catch (error) {
+            error_callback(error);
+        }
+    }
+
 }
 
 class Videos {
@@ -150,6 +220,35 @@ class Videos {
 }
 
 class Users {
+    async is_report_voted_on(user_id, video_id, report_id, callback) {
+        let found = false;
+        let upvoted_reports = await firestore_client.db.collection("users")
+        .doc(user_id).collection("upvoted_reports").get();
+        let upvoted_docs = await upvoted_reports.docs;
+        await upvoted_docs.forEach((vote) => {
+            if (vote.data()['video_id'] == video_id && vote.data()['report_id'] == report_id) {
+                console.log(`found upvote ${vote.id}`);
+                found = true;
+                callback(true, "upvoted_reports", vote.id);
+            }
+        });
+        if (!found) {
+            let downvoted_reports = await firestore_client.db.collection("users")
+            .doc(user_id).collection("downvoted_reports").get();
+            let downvoted_docs = await downvoted_reports.docs;
+            await downvoted_docs.forEach((vote) => {
+                if (vote.data()['video_id'] == video_id && vote.data()['report_id'] == report_id) {
+                    found = true;
+                    console.log(`found downvote ${vote.id}`);
+                    callback(true, "downvoted_reports", vote.id);
+                }
+            });
+        }
+        if (!found) {
+            callback(false, null, null);
+        }
+    }
+
     async is_user_there(email_id, callback) {
         let snapshot = await firestore_client.db.collection("users").get();
         let found = false;
@@ -168,9 +267,25 @@ class Users {
     }
 // collection = upvoted_reports || downvoted_reports
     add_vote(user_id, collection, video_id, report_id) {
-        firestore_client.collection("users").doc(user_id).collection(collection).add({
-            video_id: video_id,
-            report_id: report_id
+        this.is_report_voted_on(user_id, video_id, report_id, (report_is_voted_on, voted_collection, vote_id) => {
+            if (!report_is_voted_on) {
+                firestore_client.db.collection("users").doc(user_id).collection(collection).add({
+                    video_id: video_id,
+                    report_id: report_id
+                });
+                return [true, "added vote"]        
+            } else if (voted_collection == collection) {
+                return [false, "already voted"];
+            } else {
+                firestore_client.db.collection("users").doc(user_id)
+                .collection(voted_collection).doc(vote_id).delete();
+
+                firestore_client.db.collection("users").doc(user_id).collection(collection).add({
+                    video_id: video_id,
+                    report_id: report_id
+                });
+                return [true, "vote switched"]
+            }
         });
     }
 }
